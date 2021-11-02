fbec437ab30f8b922caa60921e829266b2da15fb
ann-mb <37158166+ANN-MB@users.noreply.github.com>
2021-11-02 22:36:18 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
 /* REVEALS THE MAP */
+const mabox = document.getElementById("mapbox"),
+	  cats = document.getElementById("categories"),
+	  cars = document.getElementById("caretakers"),
+	  city = document.getElementById("city"),
+	  carc = document.getElementById("careContainer");
 document.addEventListener("click", function(evt) {
-		if(evt.target.className == "tomap") {
-			document.getElementById("mapbox").className += " deploy"
-		} else if(evt.target.className !== "mapbox") {
-			document.getElementById("mapbox").className = "mapbox"
-		} 
+  if(evt.target.className == "tomap") {
+    mabox.className += " deploy"
+  } else if(evt.target.className !== "mapbox") {
+    mabox.className = "mapbox"
+  } 
 }, false);
 document.getElementById("exit").addEventListener("click", function(evt) {
-	document.getElementById("mapbox").className = "mapbox"
+  mabox.className = "mapbox"
 }, false);
 
 /* SCROLL TO TOP */
@@ -24,37 +29,32 @@ document.getElementById("totop").addEventListener("click", function(){
 }, false);
 
 /* SORTING FUNCTION */
-var chk = document.getElementsByTagName("article"),
-	chkl = chk.length,
-	cats = document.getElementById("categories"),
-	cars = document.getElementById("caretakers"),
-	city = document.getElementById("city"),
-	carc = document.getElementById("careContainer");
-
+var art = document.getElementsByTagName("article"),
+    artl = art.length;
 function sort() {
-	if (cats.value == "care") {
-		carc.style.display = "inline-block"
-	} else {
-		carc.style.display = "none"
-	}
-	var nb = 0;
-	for(let i = 0; i < chkl; i++) {
-		if(chk[i].getAttribute('data-hashtag').indexOf(cats.value) !== -1 && chk[i].getAttribute('data-hashtag').indexOf(cars.value) !== -1 && chk[i].getAttribute('data-hashtag').indexOf(city.value) !== -1) {
-			chk[i].style.display = "block";
-			nb++
-		} else {
-			chk[i].style.display = "none"
-		}
-	}
-	document.getElementById("numbr").innerHTML = "("+nb+" résultats)";
-	nb = 0;
+  if (cats.value == "care") {
+    carc.style.display = "inline-block"
+  } else {
+    carc.style.display = "none"
+  }
+  var nb = 0;
+  for(let i = 0; i < artl; i++) {
+    if(art[i].getAttribute('data-hashtag').indexOf(cats.value) !== -1 && art[i].getAttribute('data-hashtag').indexOf(cars.value) !== -1 && art[i].getAttribute('data-hashtag').indexOf(city.value) !== -1) {
+      art[i].style.display = "block";
+      nb++
+    } else {
+      art[i].style.display = "none"
+    }
+  }
+  document.getElementById("numbr").innerHTML = "("+nb+" résultats)";
+  nb = 0;
 }	
 
 cats.addEventListener("change", function(){
-	if (this.value !== "care") {
-		cars.selectedIndex = 0
-	}
-	sort()
+  if (this.value !== "care") {
+    cars.selectedIndex = 0
+  }
+  sort()
 }, false);	
 cars.addEventListener("change", sort, false);
 city.addEventListener("change", sort, false);
