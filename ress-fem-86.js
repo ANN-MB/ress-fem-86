66a07d1618cca9615c20006782cc73a5a5fb44e2
ann-mb <37158166+ANN-MB@users.noreply.github.com>
2021-11-25 20:34:44 +0100

    Add files via upload

--- ress-fem-86.js
+++ ress-fem-86.js
@@ -1,61 +1,65 @@
-/* REVEALS THE MAP */
 const mbox = document.getElementById("mapbox"),
-      mmap = document.getElementById("mymap"),
       cate = document.getElementById("categories"),
       care = document.getElementById("caretakers"),
       city = document.getElementById("city"),
       carc = document.getElementById("careContainer"),
-      ttop = document.getElementById("totop"),
       nmbr = document.getElementById("numbr"),
-	  impo = document.getElementById("important");
+	  impo = document.getElementById("important"),
+	  sort = function() {
+        var nb = 0;
+        for (let i = 0; i < artl; i++) {
+          if (art[i].getAttribute('data-hashtag').indexOf(cate.value) !== -1 && art[i].getAttribute('data-hashtag').indexOf(care.value) !== -1 && art[i].getAttribute('data-hashtag').indexOf(city.value) !== -1) {
+            art[i].style.display = "block";
+            nb++
+          } else {
+            art[i].style.display = "none"
+          }
+        }	
+        nmbr.innerHTML = 0 == nb ? "(aucun&nbsp;r&eacute;sultat)" : 1 == nb ? "(1&nbsp;r&eacute;sultat)" : "(" + nb + "&nbsp;r&eacute;sultats)";
+      },
+	  sAll = function(a)  {
+        if (document.selection) {
+          var range = document.body.createTextRange();
+          range.moveToElementText(a);
+          range.select();
+        } else if (window.getSelection) {
+          var range = document.createRange();
+          range.selectNode(a);
+          window.getSelection().addRange(range);
+        }
+      },
+	  closeMap = function() {
+		mbox.className = "mapbox";
+        document.getElementById("exit").blur();
+        stock.focus();
+        stock = void 0;
+	  }
 var art = document.getElementsByTagName("article"),
     artl = art.length,
     stock,
-	sort = function() {
-  var nb = 0;
-  for (let i = 0; i < artl; i++) {
-    if (art[i].getAttribute('data-hashtag').indexOf(cate.value) !== -1 && art[i].getAttribute('data-hashtag').indexOf(care.value) !== -1 && art[i].getAttribute('data-hashtag').indexOf(city.value) !== -1) {
-      art[i].style.display = "block";
-      nb++
-    } else {
-      art[i].style.display = "none"
-    }
-  }
-  if (nb == 0) {
-    nmbr.innerHTML = "(aucun&nbsp;r\u00e9sultat)";
-  } else if (nb == 1) {
-      nmbr.innerHTML = "(1&nbsp;r\u00e9sultat)";
-  } else {
-      nmbr.innerHTML = "(" + nb + "&nbsp;r\u00e9sultats)";
-  }
-};
-
-document.addEventListener("click", function(evt) {
-  if (evt.target.className == "tomap") {
+	clicked;
+document.addEventListener("click", function(e) {
+  var t = e.target;
+  if (t.className == "tomap") {
     mbox.className += " deploy";
-    stock = evt.target;
+    stock = t;
     document.getElementById("exit").focus();
   }
-  if (evt.target.className == "goto") {
-    evt.preventDefault();
-    var trad = evt.target.href.replace(/(.+)#([A-Za-z0-9]+)/,"$2"),
+  if (t.className == "goto") {
+    e.preventDefault();
+    var trad = t.href.replace(/(.+)#([A-Za-z0-9]+)/,"$2"),
         elem = document.getElementById(trad);
 	elem.style.display = "block";
 	elem.scrollIntoView();
   }
+  var a = t.closest("article");
+  null !== a ? a !== clicked && (clicked = a) : clicked = null;
 }, false);
 document.getElementById("quitter").addEventListener("click", function() {
   impo.style.display = "none";
   window.localStorage.setItem("modale", true);
 }, false);
-
-document.getElementById("exit").addEventListener("click", function() {
-  mbox.className = "mapbox";
-  document.getElementById("exit").blur();
-  stock.focus();
-  stock = void 0;
-}, false);
-
+document.getElementById("exit").addEventListener("click", closeMap, false);
 care.addEventListener("change", sort, false);
 city.addEventListener("change", sort, false);
 cate.addEventListener("change", function() {
@@ -63,13 +67,20 @@ cate.addEventListener("change", function() {
   "phon" == this.value && (city.selectedIndex = 0);
   sort();
 }, false);
-
 window.addEventListener("scroll", function() {
+  var ttop = document.getElementById("totop");
   document.body.scrollTop > 600 || document.documentElement.scrollTop > 600 ? ttop.style.display = "block" : ttop.style.display = "none";
 }, false);
-
 window.addEventListener("DOMContentLoaded", function() {
   cate.selectedIndex = care.selectedIndex = city.selectedIndex = 0;
-  nmbr.innerHTML = "(" + artl + "&nbsp;r\u00e9sultats)";
+  nmbr.innerHTML = "(" + artl + "&nbsp;r&eacute;sultats)";
   window.localStorage.getItem("modale") || (impo.style.display = "block");
+},false);
+document.addEventListener("keydown", function(e){
+  var key = e.keyCode || e.which;
+  if (clicked && true == e.ctrlKey && 65 == key) {
+    e.preventDefault();
+	null !== clicked && sAll(clicked);
+  }
+  if (27 == key) closeMap()
 },false);
