8ce64ee3b8934d3396ba157ba0be5c9f39d96c37
ann-mb <37158166+ANN-MB@users.noreply.github.com>
2021-12-20 13:50:02 +0100

    Update map.js

--- js/map.js
+++ js/map.js
@@ -1,30 +1,38 @@
 var map = L.map('mymap', {
   zoomSnap: 0.5
-}).fitBounds([
-  [46.19709708576172, -0.9106632928479332],
-  [47.05997975442446, 1.7422818877878934]
-]).setView([46.63025794928896, 0.41580929746996015], 9.5);
+}).setView([46.63025794928896, 0.41580929746996015], 9.5);
 
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+var 
+light = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
   attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-  minZoom: 9
-}).addTo(map);
-
-L.Map = L.Map.extend({
-  openPopup: function(popup) {
-    this.closePopup();
-    this._popup = popup;
-    return this.addLayer(popup).fire('popupopen', {
-      popup: this._popup
-    });
-  }
+  minZoom: 9.5,
+  maxZoom: 18
+}),
+dark = L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', {
+  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+  minZoom: 9,
+  maxZoom: 18
 });
+  
+if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+  dark.addTo(map);
+} else {
+  light.addTo(map);
+}
+
+window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', function(e) {
+  var newC = e.matches ? "dark" : "light",
+      oldC = e.matches ? "light" : "dark";
+  map.removeLayer(window[oldC]);
+  map.addLayer(window[newC]);
+});
+
 
 var popmaps = function(feature, layer) {
   var prop = feature.properties,
     popUp = "<h2>" + prop.title + "</h2>" +
     "<div class=\"desc\">" + (prop.description || "").replace(/\[\[(.+?)\|(.+?)\]\]/gi, "<a href=\"$1\">$2</a>") + "</div>" +
-    "<div class=\"num\">" + (prop.phone || "").replace(/\[\[(.+?)\|(.+?)\]\]/gi, "<a href=\"$1\">$2</a>").replace(/(?:\+33|0)([0-9 ]+)/g, "<a href=\"tel:+33$1\">0$1</a>").replace(/0([0-9])([0-9]{2})([0-9]{2})([0-9]{2})([0-9]{2})/g, "0$1 $2 $3 $4 $5") + "</div>" +
+    "<div class=\"num\">" + (prop.phone || "").replace(/\[\[(.+?)\|(.+?)\]\]/gi, "<a href=\"$1\">$2</a>").replace(/(?:\+33|0)([0-9 ]{3,})/g, "<a href=\"tel:+33$1\">0$1</a>").replace(/0([0-9])([0-9]{2})([0-9]{2})([0-9]{2})([0-9]{2})/g, "0$1 $2 $3 $4 $5") + "</div>" +
     "<div class=\"mail\">" + (prop.email || "").replace(/([A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,})/gi, "<a href=\"mailto:$1\">$1</a>") + "</div>" +
     "<div class=\"web\">" + (prop.url || "").replace(/(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})/gi, "<a href=\"$1\">$1</a>") + "</div>" +
     "<div class=\"loc\">" + (prop.address || "") + "</div>" +
@@ -42,16 +50,15 @@ var popmaps = function(feature, layer) {
     "<div class=\"info\">" + (prop.info || "").replace(/\[\[(.+?)\|(.+?)\]\]/gi, "<a href=\"$1\">$2</a>") + "</div>" +
     "<div class=\"avis\">" + (prop.avis || "").replace(/\[\[(.+?)\|(.+?)\]\]/gi, "<a href=\"$1\">$2</a>") + "</div>";
   layer.bindPopup(String(popUp));
-}
-
-var main = L.geoJson(geojson, {
+},
+main = L.geoJson(geojson, {
   pointToLayer: function(feature, latlng) {
     var icon = L.divIcon({
-      iconSize: [35, 35],
-      iconAnchor: [17, 35],
+      iconSize: [35,35],
+      iconAnchor: [17,35],
       className: 'm-' + (feature.properties.category || "default"),
-      popupAnchor: [0, 0],
-      html: '<div class="marker-pin"></div>'
+      popupAnchor: [0,0],
+      html: '<div class="marker-pin"></div><div class="shadow"></div>'
     });
     return L.marker(latlng, {
       icon: icon,
@@ -61,38 +68,25 @@ var main = L.geoJson(geojson, {
   onEachFeature: popmaps
 }).addTo(map);
 
-/*https://www.datavis.fr/index.php?page=leaflet-firstmap*/
+map.fitBounds(main.getBounds());
 
-var cats = [];
-
-function getCat(cats, cat) {
-  for (var i = cats.length; i--;) {
-    if (cats[i]["label"] === cat) {
-      return cats[i];
-    }
-  }
-  return;
-}
-for (var i = 0; i < geojson.length; i++) {
-  var cat = getCat(cats, geojson[i].properties.category);
-    
-  if (cat === undefined) {
-    cat = {
-      "id": "m-"+geojson[i].properties.category,
-      "label": geojson[i].properties.category
-    }
-    cats.push(cat);
-  }
-}
-
-/** add menu */
 var command = L.control({position: 'topright'});
+
 command.onAdd = function(map) {
   var div = L.DomUtil.create('div', 'command');
-  div.innerHTML += '<h4>Filtre</h4>';
-  for (var i = 0; i < cats.length; i++) {
-    div.innerHTML += '<div><input id="' + cats[i]["id"] + '" type="checkbox" class="chk-filter" checked />' + cats[i]["label"] + '</div>';
-  }
+  div.innerHTML += '<h3>Filtre</h3>'
+  + '<label for="m-menses"><input id="m-menses" type="checkbox" class="chk-filter" checked />Boîtes à don menstruelles</label>'
+  + '<label for="m-host"><input id="m-host" type="checkbox" class="chk-filter" checked />Hébergement</label>'
+  + '<label for="m-testing"><input id="m-testing" type="checkbox" class="chk-filter" checked />Dépistage IST</label>'
+  + '<label for="m-doctor"><input id="m-doctor" type="checkbox" class="chk-filter" checked />Soignant&middot;es</label>'
+  + '<label for="m-sport"><input id="m-sport" type="checkbox" class="chk-filter" checked />Sport</label>'
+  + '<label for="m-activism"><input id="m-activism" type="checkbox" class="chk-filter" checked />Militer</label>'
+  + '<label for="m-health"><input id="m-health" type="checkbox" class="chk-filter" checked />Santé</label>'
+  + '<label for="m-culture"><input id="m-culture" type="checkbox" class="chk-filter" checked />Culturel</label>'
+  + '<label for="m-support"><input id="m-support" type="checkbox" class="chk-filter" checked />Groupe de parole</label>'
+  + '<label for="m-official"><input id="m-official" type="checkbox" class="chk-filter" checked />Élues</label>'
+  + '<label for="m-solidarity"><input id="m-solidarity" type="checkbox" class="chk-filter" checked />Solidarité</label>'
+  + '<label for="m-right"><input id="m-right" type="checkbox" class="chk-filter" checked />Droit & Justice</label>'
   return div;
 };
 command.addTo(map);
@@ -100,8 +94,7 @@ command.addTo(map);
 /* add events to all checkbox */
 document.addEventListener("click", function(e){
   if (e.target.className == "chk-filter") {
-     var el = document.getElementsByClassName(e.target.id),
-         ell = el.length;
+     var el = document.getElementsByClassName(e.target.id);
     if (e.target.checked) {
       [...el].forEach(x => (x.style.display = "block"));
     } else {
